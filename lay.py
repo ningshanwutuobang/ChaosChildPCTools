4020463cced346f9fe02556ca4cf6349bd87324c
ningshanwutuobang <ningshanwutuobang@gmail.com>
2018-02-22 14:10:19 +0800

    add support for SG0

--- lay.py
+++ lay.py
@@ -13,18 +13,20 @@ def main(filename,piece=True,Total=True):
 		return
 	#using zlib to decompress
 	b=b''
-	decompress = zlib.decompressobj()
-	data = infile.read(1024)
-	while data:
+	sg0=False
+	data = infile.read()
+	if data[:2]==b'\x78\x9c':
+		decompress = zlib.decompressobj()
 		b+=decompress.decompress(data)
-		data = infile.read(1024)
+	else :
+		sg0=True
+		b+=data
 	#creat a folder 
 	p=0
 	folder=filename[0:filename.index('_.')]
 	os.system("mkdir "+folder)
 	#the head of the file is the number of images and piece blocks 
 	imageNum,pieceNum = struct.unpack("<2I",b[p:p+8])
-	print("Image have ",imageNum," parts.")
 	p+=8
 	#image information following
 	partNum=[]
@@ -48,8 +50,11 @@ def main(filename,piece=True,Total=True):
 	can=Image.new ("RGBA", (4000,2000))
 	#get the images
 	j=0
+	l=len(b)
 	for i in range(pieceNum+1):
-		f1,f2,f3,f4=struct.unpack('<4f',b[p:p+16])
+		f1,f2,f3,f4=0,0,0,0
+		if p<l:
+			f1,f2,f3,f4=struct.unpack('<4f',b[p:p+16])
 		f1,f2=f1+2000.0,f2+1000.0  #center
 		p+=16
 		if i==partNum[j]:
@@ -63,7 +68,7 @@ def main(filename,piece=True,Total=True):
 				can=Image.new("RGBA", (4000,2000))#reset the Canvas
 			j+=1
 			minx,miny,maxx,maxy=int(f1),int(f2),int(f1),int(f2)
-		tmp=img.crop((int(f3)-1,int(f4)-1,int(f3)+31,int(f4)+31))#piece block
+		tmp=img.crop((int(f3)-1,int(f4)-1,int(f3)+29,int(f4)+29))#piece block
 		x,y=int(f1),int(f2)
 		can.paste(tmp,(x,y))
 		minx,maxx=min(minx,x),max(maxx,x)
@@ -88,10 +93,10 @@ def main(filename,piece=True,Total=True):
 						x,y,z,w=pos[path[k]]
 						minx,maxx=min(minx,x),max(maxx,x)
 						miny,maxy=min(miny,y),max(maxy,y)
-						can.paste(pngs[path[k]],(x,y))
+						can.paste(pngs[path[k]],(x,y),mask=pngs[path[k]])
 				part=can.crop((minx,miny,maxx,maxy))
-				part.save(folder+"/"+filename[0:-4]+"_"+str(j)+".png")
-				print("\t"+filename[0:-4]+"_"+str(j)+".png")
+				part.save(folder+"/"+filename[0:-4]+str(j)+".png")
+				print("\t"+filename[0:-4]+str(j)+".png")
 				j+=1
 			if i<imageNum:
 				last=int(partTree[i][3]/0x10)
